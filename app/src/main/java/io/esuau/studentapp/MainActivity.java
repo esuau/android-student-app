785257fcff63741b4e7b1aa8e7a0202d89a5f2ee
esuau <evan.suau@etu.u-pec.fr>
2018-09-21 16:08:42 +0200

    Navigate to StudentListActivity from MainActivity

--- app/src/main/java/io/esuau/studentapp/MainActivity.java
+++ app/src/main/java/io/esuau/studentapp/MainActivity.java
@@ -10,17 +10,26 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private final int ADD_STUDENT_RESULT = 1;
+    private final int SHOW_STUDENTS_RESULT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button button = findViewById(R.id.button1);
-        button.setOnClickListener(new View.OnClickListener() {
+        final Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddStudentActivity.class);
                 startActivityForResult(intent, ADD_STUDENT_RESULT);
+            }
+        });
+
+        final Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddStudentActivity.class);
+                startActivityForResult(intent, SHOW_STUDENTS_RESULT);
             }
         });
     }
@@ -28,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == ADD_STUDENT_RESULT) {
+        if (resultCode == ADD_STUDENT_RESULT || resultCode == SHOW_STUDENTS_RESULT) {
             setResult(RESULT_OK);
             finish();
         }
