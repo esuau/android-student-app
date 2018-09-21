e20b21a74b71df8b19f5b7ca620053ceccf9582a
esuau <evan.suau@etu.u-pec.fr>
2018-09-21 14:03:20 +0200

    Use startActivityForResult for navigation to AddStudentActivity

--- app/src/main/java/io/esuau/studentapp/MainActivity.java
+++ app/src/main/java/io/esuau/studentapp/MainActivity.java
@@ -1,12 +1,15 @@
 package io.esuau.studentapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int ADD_STUDENT_RESULT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +20,16 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddStudentActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, ADD_STUDENT_RESULT);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == ADD_STUDENT_RESULT) {
+            finish();
+        }
     }
 }
