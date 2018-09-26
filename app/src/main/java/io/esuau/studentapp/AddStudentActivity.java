da30b408cafc9f49c2c45eaf7e7ece6aa196efe1
esuau <evan.suau@etu.u-pec.fr>
2018-09-26 16:04:07 +0200

    Add date picker modal to AddStudentActivity

--- app/src/main/java/io/esuau/studentapp/AddStudentActivity.java
+++ app/src/main/java/io/esuau/studentapp/AddStudentActivity.java
@@ -1,13 +1,51 @@
 package io.esuau.studentapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
 
-public class AddStudentActivity extends AppCompatActivity {
+import java.util.Calendar;
+
+public class AddStudentActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button button;
+
+    private TextView textDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_student);
+
+        textDate = findViewById(R.id.textdate);
+
+        button = findViewById(R.id.datepickerbutton);
+        button.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v) {
+        if (v == button) {
+            final Calendar c = Calendar.getInstance();
+            int mYear = c.get(Calendar.YEAR);
+            int mMonth = c.get(Calendar.MONTH);
+            int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                    new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            String dateStr = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                            textDate.setText(dateStr);
+                        }
+                    }, mYear, mMonth, mDay);
+            datePickerDialog.show();
+        }
+
+    }
+
 }
