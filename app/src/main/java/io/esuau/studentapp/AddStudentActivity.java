940a29c5c5da37c87ff5176dfca32f7303661af1
esuau <evan.suau@etu.u-pec.fr>
2018-09-27 12:15:38 +0200

    Add student to global list

--- app/src/main/java/io/esuau/studentapp/AddStudentActivity.java
+++ app/src/main/java/io/esuau/studentapp/AddStudentActivity.java
@@ -4,15 +4,16 @@ import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.DatePicker;
-import android.widget.TextView;
+import android.widget.*;
+import io.esuau.studentapp.definition.Student;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddStudentActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button button;
+    Button datePickerButton;
+    Button addStudentButton;
 
     private TextView textDate;
 
@@ -23,13 +24,16 @@ public class AddStudentActivity extends AppCompatActivity implements View.OnClic
 
         textDate = findViewById(R.id.textdate);
 
-        button = findViewById(R.id.datepickerbutton);
-        button.setOnClickListener(this);
+        datePickerButton = findViewById(R.id.datepickerbutton);
+        datePickerButton.setOnClickListener(this);
+
+        addStudentButton = findViewById(R.id.addstudentbutton);
+        addStudentButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        if (v == button) {
+        if (v == datePickerButton) {
             final Calendar c = Calendar.getInstance();
             int mYear = c.get(Calendar.YEAR);
             int mMonth = c.get(Calendar.MONTH);
@@ -44,8 +48,29 @@ public class AddStudentActivity extends AppCompatActivity implements View.OnClic
                         }
                     }, mYear, mMonth, mDay);
             datePickerDialog.show();
-        }
+        } else if (v == addStudentButton) {
+            EditText inputName = findViewById(R.id.input_name);
+            String name = inputName.getText().toString();
 
+            EditText inputFirstName = findViewById(R.id.input_first_name);
+            String firstName = inputFirstName.getText().toString();
+
+            RadioGroup inputGenderGroup = findViewById(R.id.input_gender);
+            RadioButton inputGender = findViewById(inputGenderGroup.getCheckedRadioButtonId());
+            String gender = inputGender.getText().toString();
+
+            EditText inputEmail = findViewById(R.id.input_email);
+            String email = inputEmail.getText().toString();
+
+            EditText inputGroup = findViewById(R.id.input_group);
+            String group = inputGroup.getText().toString();
+
+            Switch inputStatus = findViewById(R.id.input_status);
+            boolean status = inputStatus.isChecked();
+
+            Students application = (Students) getApplication();
+            application.addStudent(new Student(name, firstName, gender, email, new Date(), group, status));
+        }
     }
 
 }
--- app/src/main/java/io/esuau/studentapp/StudentListActivity.java
+++ app/src/main/java/io/esuau/studentapp/StudentListActivity.java
@@ -1,15 +1,14 @@
 package io.esuau.studentapp;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import io.esuau.studentapp.definition.Student;
 
-import java.util.Date;
 import java.util.LinkedList;
 
 public class StudentListActivity extends AppCompatActivity {
@@ -20,10 +19,9 @@ public class StudentListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_list);
-        final LinkedList<Student> students = new LinkedList<>();
-        students.add(new Student("Suau", "Evan", "Homme", "evan.suau@etu.u-pec.fr", new Date(842997600000L), "SINCC", false));
-        students.add(new Student("Doe", "Jane", "Femme", "jane@example.org", new Date(), "SID", false));
-        students.add(new Student("Doe", "John", "Homme", "john@example.com", new Date(), "SID", false));
+        Students application = (Students) getApplication();
+        final LinkedList<Student> students = application.getStudents();
+
         StudentAdapter adapter = new StudentAdapter(getApplicationContext(), R.layout.activity_student_list, students);
         ListView studentListView = findViewById(R.id.student_list);
         studentListView.setAdapter(adapter);
