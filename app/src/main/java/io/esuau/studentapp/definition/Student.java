bf39f8df7c714765a993fede7eeee22d1bc99b3b
esuau <evan.suau@etu.u-pec.fr>
2018-11-26 12:13:09 +0100

    refactor(student): Use parcelable for Student objects

--- app/src/main/java/io/esuau/studentapp/definition/Student.java
+++ app/src/main/java/io/esuau/studentapp/definition/Student.java
@@ -1,16 +1,17 @@
 package io.esuau.studentapp.definition;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.io.Serializable;
 import java.util.Date;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Student implements Serializable {
+public class Student implements Parcelable {
 
     private String name;
 
@@ -25,5 +26,43 @@ public class Student implements Serializable {
     private String group;
 
     private boolean status;
+
+    public Student(Parcel in) {
+        name = in.readString();
+        firstName = in.readString();
+        gender = in.readString();
+        email = in.readString();
+        birthDate = (Date) in.readSerializable();
+        group = in.readString();
+        status = in.readByte() != 0;
+    }
+
+    public static final Parcelable.Creator<Student> CREATOR = new Parcelable.Creator<Student>() {
+        @Override
+        public Student createFromParcel(Parcel source) {
+            return new Student(source);
+        }
+
+        @Override
+        public Student[] newArray(int size) {
+            return new Student[size];
+        }
+    };
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(firstName);
+        dest.writeString(gender);
+        dest.writeString(email);
+        dest.writeSerializable(birthDate);
+        dest.writeString(group);
+        dest.writeByte((byte) (status ? 1 : 0));
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
 
 }
