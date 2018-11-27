5af86f48d573b1c313fe11bdf151bf634236003f
esuau <evan.suau@etu.u-pec.fr>
2018-11-27 12:54:55 +0100

    chore(global var): Remove useless imports

--- app/src/main/java/io/esuau/studentapp/AddStudentActivity.java
+++ app/src/main/java/io/esuau/studentapp/AddStudentActivity.java
@@ -3,7 +3,6 @@ package io.esuau.studentapp;
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcel;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
